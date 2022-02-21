a88c788d0c82117349e4f30aa35342e0a55e42dd
Koko-Dev <koko.webdev@gmail.com>
2022-02-21 10:49:52 -0500

    In index.js, create a path_name for the store and the item page to res.end when client opens '/', '/store', and '/item'

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
+const path = require("path");
+
+
 
 const server = http.createServer((req, res) => {
-	res.end('Hello from the server!');
+	console.log(req.url);
+
+	const path_name = req.url;
+
+	if (path_name === '/store' || path_name === '/') {
+		res.end('Welcome to the Store!');
+	} else if (path_name === '/item') {
+		res.end('This is your Item Page!')
+	}
+
+
+	// res.end('Hello from the server!');
 });
 
 server.listen(8000, '127.0.0.1', () => {
