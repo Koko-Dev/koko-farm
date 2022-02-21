2e14d65c4b6db2d62a246ca17e355e6c5c7aa235
Koko-Dev <koko.webdev@gmail.com>
2022-02-21 14:11:25 -0500

    Create handlers/template_replacement.js; export a function that will take a unique template variable and replace it with JSON data throughout the entire template; every instance of each variable will be replaced as indicated on each template

---
+++ handlers/template_replacement.js
@@ -0,0 +1,14 @@
+module.exports = (template, item) => {
+	let replacement = template.replace(/{%ITEM-NAME%}/g, item.itemName);
+	replacement = replacement.replace(/{%IMAGE%}/g, item.image);
+	replacement = replacement.replace(/{%FROM%}/g, item.from);
+	replacement = replacement.replace(/{%NUTRIENTS%}/g, item.nutrients);
+	replacement = replacement.replace(/{%QUANTITY%}/g, item.quantity);
+	replacement = replacement.replace(/{%PRICE%}/g, item.price);
+	replacement = replacement.replace(/{%DESCRIPTION%}/g, item.description);
+	replacement = replacement.replace(/{%ID%}/g, item.id);
+
+	if (!item.organic) replacement = replacement.replace(/{%ID%}/g, 'not-organic');
+
+	return replacement;
+}
