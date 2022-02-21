39118cace4d9f7a7621b14f2274792f236c085f7
Koko-Dev <koko.webdev@gmail.com>
2022-02-21 12:07:42 -0500

    In index.js, add status codes and headers to each route, res.end response as <h1> titles to ensure we are reaching each page as well as a 404 for incorrect routes

--- index.js
+++ index.js
@@ -3,22 +3,27 @@ const http = require('http');
 const url = require('url');
 const path = require("path");
 
-
-
 const server = http.createServer((req, res) => {
 	console.log(req.url);
 
 	const path_name = req.url;
 
 	if (path_name === '/store' || path_name === '/') {
-		res.end('Welcome to the Store!');
+		res.writeHead(200, {
+			"Content-type": "text/html"
+		});
+		res.end('<h1>Welcome to the Store!</h1>');
 	} else if (path_name === '/item') {
-		res.end('This is your Item Page!')
+		res.writeHead(200, {
+			"Content-type": "text/html"
+		});
+		res.end('<h1>This is your Item Page!</h1>')
 	} else {
-		res.writeHead(404);
-		res.end('Page Not Found!');
+		res.writeHead(404, {
+			"Content-type": "text/html"
+		});
+		res.end('<h1>Page Not Found!</h1>');
 	}
-
 
 	// res.end('Hello from the server!');
 });
